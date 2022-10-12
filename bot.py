b725037de6d88b2cd603be93f192965eee4dcbaf
Caalek <adamcal@protonmail.com>
2022-10-12 15:30:39 +0200

    fix json error

--- bot.py
+++ bot.py
@@ -11,13 +11,11 @@ token = os.environ["BOT_TOKEN"]
 prefix = "g!"
 bot = lightbulb.BotApp(token=token, prefix=prefix)
 
-
 def get_random_quote():
     with open("quotes.json", "r") as f:
         quote_array = json.load(f)
     return random.choice(quote_array)
     
-
 @bot.command
 @lightbulb.command("cytat", "Losowy cytat z Wiedźmina.")
 @lightbulb.implements(SlashCommand, PrefixCommand)
